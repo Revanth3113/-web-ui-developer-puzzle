6266508fec668ac8535e8a36541d20c7c63e2406
Mandala, Revanth - Contingent Worker <Revanth.Mandala@optimityadvisors.com>
2020-08-19 13:10:27 -0400

    e2e test

--- apps/okreads-e2e/src/specs/reading-list.spec.ts
+++ apps/okreads-e2e/src/specs/reading-list.spec.ts
@@ -17,4 +17,34 @@ describe('When: I use the reading list feature', () => {
       )
     );
   });
+
+  it('Then: Reading completed action', async () => {
+    await browser.get('/');
+    await browser.wait(
+      ExpectedConditions.textToBePresentInElement($('tmo-root'), 'okreads')
+    );
+    const form = await $('form');
+    const input = await $('input[type="search"]');
+    await input.sendKeys('java');
+    await form.submit();
+    const wantToRead = await $('[data-testing="wantToRead"]');
+    await wantToRead.click();
+
+
+    const readingListToggle = await $('[data-testing="toggle-reading-list"]');
+    await readingListToggle.click();
+
+    const readingCompleted = await $('[data-testing="readingCompleted"]');
+    await readingCompleted.click();
+
+    
+    await browser.wait(
+      ExpectedConditions.textToBePresentInElement(
+        $('[data-testing="finished"]'),
+        'Finished'
+      )
+    );
+  });  
+  
+  
 });
